fc6c995447313a857db9df2c426b9076934e738a
Vitalii Khivrych <khivr.vitali@gmail.com>
2022-05-10 18:05:51 +0300

    for inspection

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,70 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryContainerRef = document.querySelector('.gallery');
+const cardsMarkup = createGalleryCardMarkup(galleryItems);
+
+galleryContainerRef.insertAdjacentHTML('beforeend', cardsMarkup);
+
+galleryContainerRef.addEventListener('click', onImageGalleryClick);
+
+let getOpenModalRef;
+let getCloseModalRef;
+
+function createGalleryCardMarkup(galleryItems) {
+  const markup = galleryItems
+    .map(({ description, original, preview }) => {
+      return `<div class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                  <img
+                    class="gallery__image"
+                    src="${preview} "
+                    data-source="${original} "
+                    alt="${description} "
+                  />
+                </a>
+              </div>`;
+    })
+    .join('');
+  return markup;
+}
+
+function onImageGalleryClick(event) {
+  event.preventDefault();
+  // console.log(event.target.dataset.source);
+
+  const imgSource = event.target.dataset.source;
+
+  createModal(imgSource);
+
+  openModal();
+}
+
+function createModal(source) {
+  const instance = basicLightbox.create(`
+    <img src="${source}" width="800" height="600">`);
+
+  getOpenModalRef = instance.show;
+  getCloseModalRef = instance.close;
+}
+
+function onCloseForEsc(event) {
+  // console.log(event);
+  if (event.code === 'Escape') {
+    closeModal();
+  }
+}
+
+function closeModal() {
+  window.removeEventListener('keydown', onCloseForEsc);
+
+  getCloseModalRef();
+}
+
+function openModal() {
+  window.addEventListener('keydown', onCloseForEsc);
+
+  getOpenModalRef();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const gallaryRef = document.querySelector('.gallery');
+const galleryMarkup = createGalleryCardsMarkup(galleryItems);
+
+gallaryRef.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryCardsMarkup(galleryItems) {
+  const markup = galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+              <img class="gallery__image" src="${preview}" alt="${description}"/>
+            </a>`;
+    })
+    .join('');
+  return markup;
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  // captions: true,
+  // captionSelector: 'img',
+  // captionType: 'attr',
+  captionsData: 'alt',
+  // captionPosition: 'bottom',
+  captionDelay: 250,
+});
